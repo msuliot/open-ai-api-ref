223cbdb379d67e4d25e2223ce9dbcfa6c2a73351
Suliot <suliot@msn.com>
2023-07-10 09:22:40 -0700

    created helper

--- Helper.py
+++ Helper.py
@@ -1,4 +1,8 @@
 import openai
+import webbrowser
+
+def open_image(url):
+    webbrowser.open(url)
 
 def get_completion(prompt, model="gpt-3.5-turbo",temperature=0): 
     try:
@@ -89,3 +93,61 @@ def create_prompt(transcript):
 
         """
     return prompt   
+
+
+def create_image(type, prompt, count=1, image_file_name="", image_file_mask=""):
+    try:
+        if type not in ["create", "edit", "variation"]:
+            raise ValueError("Invalid type parameter. Must be 'create', 'edit', or 'variation'.")
+        
+        if type == "create":
+            image = openai.Image.create(
+            prompt=prompt,
+            n=count, # The number of images to generate
+            size="1024x1024",
+            )
+            
+        if type == "edit":
+            image = openai.Image.create_edit(
+            image=open(image_file_name, "rb"), # Original image = exact size as the Mask image
+            mask=open(image_file_mask, "rb"), # Modified image with Mask = exact same size as the original image
+            prompt=prompt,
+            n=count, # The number of images to generate
+            size="1024x1024",
+            )
+            
+        if type == 'variation':
+            image = openai.Image.create_variation(
+            image=open(image_file_name, "rb"),
+            n=count, # The number of images to generate
+            size="1024x1024",
+            )
+        
+    except openai.error.APIError as e:
+        #Handle API error here, e.g. retry or log
+        print(f"OpenAI API returned an API Error: {e}")
+        pass
+    except openai.error.APIConnectionError as e:
+        #Handle connection error here
+        print(f"Failed to connect to OpenAI API: {e}")
+        pass
+    except openai.error.RateLimitError as e:
+        #Handle rate limit error (we recommend using exponential backoff)
+        print(f"OpenAI API request exceeded rate limit: {e}")
+        pass
+    except openai.error.AuthenticationError as e:
+        #Handle authentication error (e.g. invalid credentials)
+        print(f"OpenAI API request failed due to invalid credentials: {e}")
+        pass
+    except openai.error.InvalidRequestError as e:
+        #Handle invalid request error (e.g. required parameter missing)
+        print(f"OpenAI API request failed due to invalid parameters: {e}")
+        pass
+    except openai.error.ServiceUnavailableError as e:
+        #Handle service unavailable error
+        print(f"OpenAI API request failed due to a temporary server error: {e}")
+        pass
+    else:
+        # code to execute if no exception was raised
+        for item in image["data"]:
+            open_image(item["url"])
--- use_cases.py
+++ use_cases.py
@@ -1,14 +1,33 @@
 import openai
 import open_api_key
 openai.api_key = open_api_key.get_api_key()
-import Util
+import Helper
 
 #####################################################
 
-transcript = Util.audio_to_text("audio/happy.m4a") # convert audio to text
-response = Util.get_completion(Util.create_prompt(transcript)) # create prompt with text and instructions on how to respond
-print(response)
+# 1
+# transcript = Helper.audio_to_text("audio/happy.m4a") # convert audio to text
+# response = Helper.get_completion(Helper.create_prompt(transcript)) # create prompt with text and instructions on how to respond
+# print(response)
 
-transcript = Util.audio_to_text("audio/mad.m4a")
-response = Util.get_completion(Util.create_prompt(transcript))
-print(response)
+# 2
+# transcript = Helper.audio_to_text("audio/mad.m4a")
+# response = Helper.get_completion(Helper.create_prompt(transcript))
+# print(response)
+
+
+#####################################################
+
+# 3
+# Helper.create_image("create","create a picture of a dog")
+
+# 4
+# Helper.create_image("edit",
+#                     "And some mountains along the horizon, and then the sky is a nebula",
+#                     1,
+#                     "images/michael.png",
+#                     "images/transparency.png"
+#                     )
+
+# 5
+# Helper.create_image("variation","",1,"images/michael.png")
